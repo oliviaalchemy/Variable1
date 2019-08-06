95e29d970eb722bac1a7e93580ef33d3d26cf953
oliviaalchemy <olivia@alchemytechs.com>
2019-08-06 10:25:21 +0100

    Add project files.

---
+++ Variable1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Variable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Tom";
+            int characterAge;
+            characterAge = 25;         //these are both different ways to do the same thing
+            Console.WriteLine("There once was a man named " + characterName); 
+            Console.WriteLine("He was " + characterAge + " years old");
+
+            characterName = "Mike";
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
+            Console.ReadLine();
+        }
+        }
+    }
+
